9e34cf09c50a7dd82bc8ad8cce3f482bcd92d62a
Kento Nishi <kento24gs@outlook.com>
2021-05-05 22:36:52 -0700

    Removed old code and logs

--- src/App.svelte
+++ src/App.svelte
@@ -4,29 +4,23 @@
     AppBar,
     Button,
     Container,
-    Divider
-  } from "svelte-materialify/src";
-  import { GreensFctCalc, Wronskian } from "./modules/calc.js";
-  import MathInput from "./components/MathInput.svelte";
-  import MathDisplay from "./components/MathDisplay.svelte";
-  let y_1 = "",
-    y_2 = "",
-    f = "",
-    x_0 = "";
-  let answer = "";
-  // let answer2 = "";
-  let w = "";
+    Divider,
+  } from 'svelte-materialify/src';
+  import { GreensFctCalc, Wronskian } from './modules/calc.js';
+  import MathInput from './components/MathInput.svelte';
+  import MathDisplay from './components/MathDisplay.svelte';
+  let y_1 = '',
+    y_2 = '',
+    f = '',
+    x_0 = '';
+  let answer = '';
+  let w = '';
   function submitInputs() {
     try {
       answer = GreensFctCalc(y_1, y_2);
     } catch (e) {
       console.log(e);
     }
-    // try {
-    //   answer2 = y_p(y_1, y_2, f, x_0);
-    // } catch (e) {
-    //   console.log(e);
-    // }
     try {
       w = Wronskian(y_1, y_2);
     } catch (e) {
@@ -34,15 +28,58 @@
     }
   }
   function clearAll() {
-    answer = "";
-    // answer2 = "";
-    w = "";
-    y_1 = "";
-    y_2 = "";
-    // f = "";
-    // x_0 = "";
+    answer = '';
+    w = '';
+    y_1 = '';
+    y_2 = '';
   }
 </script>
+
+<MaterialApp>
+  <AppBar dense>
+    <span slot="title">Green's Function Calculator</span>
+    <div style="flex-grow:1;" />
+    <span style="margin-right: 25px">
+      Vignav Ramesh, Kento Nishi, Preston Fu, Nikhil Gargeya
+    </span>
+  </AppBar>
+  <Container class="text-left">
+    <h6 class="mt-4 mb-4">
+      Use this online tool to automatically calculate Green's functions! Given
+      the nonhomogeneous differential equation $$y'' + P(x)y' + Q(x)y =
+      f(x),$$the Green's function is given by $$G(x,t) = \frac{'{'}y_1(t)y_2(x)
+      - y_1(x)y_2(t){'}'}{'{'}W(t){'}'},$$ where \( W(t) \) is the Wronskian and
+      is given by $$W(y_1(t), y_2(t)) = \begin{'{'}vmatrix{'}'}y_1{'{'}t{'}'} & y_2{'{'}t{'}'}
+      \\ y_1'(t) & y_2'(t) \end{'{'}vmatrix{'}'}.$$ Enter the values of \(y_1\)
+      and \(y_2\) below to automatically calculate the Wronskian and Green's
+      function!
+    </h6>
+  </Container>
+  <Container class="text-center">
+    <h6 class="mt-4 mb-4">
+      \(y_1(x) =\)
+      <MathInput bind:value={y_1} />
+      , \(y_2(x) =\)
+      <MathInput bind:value={y_2} />
+      <br />
+      <br />
+      <Button class="yellow" on:click={submitInputs}>SUBMIT</Button>
+      <Button class="yellow" on:click={clearAll}>CLEAR</Button>
+      <br />
+      <br />
+      <Divider />
+
+      <br />
+      <strong>Wronskian \(W(y_1, y_2)\):</strong>
+      <MathDisplay value={w} />
+      <br />
+      <br />
+
+      <strong>Green's Function:</strong>
+      <MathDisplay value={answer} />
+    </h6>
+  </Container>
+</MaterialApp>
 
 <style>
   h3 {
@@ -63,65 +100,3 @@
     margin: 20px;
   }
 </style>
-
-<MaterialApp>
-  <AppBar dense>
-    <!-- <span slot="title">Green's Functions for Initial-Value Problems</span> -->
-    <span slot="title">Green's Function Calculator</span>
-    <div style="flex-grow:1;" />
-    <span style="margin-right: 25px">
-      Vignav Ramesh, Kento Nishi, Preston Fu, Nikhil Gargeya
-    </span>
-  </AppBar>
-  <Container class="text-left">
-    <!-- <h6 class="mt-4 mb-4">
-      Use this online tool to automatically calculate Green's functions! Given the
-      nonhomogeneous differential equation $$y'' + P(x)y' + Q(x)y = f(x)$$ with
-      homogeneous initial conditions \(y(x_0) = y'(x_0) = 0\) and complementary
-      function $$y_c = c_1y_1 + c_2y_2,$$ enter the values of \(y_1\), \(y_2\),
-      \(f\), and \(x_0\) below to automatically calculate the particular
-      solution \(y_p\).
-    </h6> -->
-    <h6 class="mt-4 mb-4">
-      Use this online tool to automatically calculate Green's functions! Given the
-      nonhomogeneous differential equation $$y'' + P(x)y' + Q(x)y = f(x),$$the Green's function is given by
-      $$G(x,t) = \frac{'{'}y_1(t)y_2(x) - y_1(x)y_2(t){'}'}{'{'}W(t){'}'},$$ where \( W(t) \) is the Wronskian and is given by
-      $$W(y_1(t), y_2(t)) = \begin{'{'}vmatrix{'}'}y_1{'{'}t{'}'} & y_2{'{'}t{'}'} \\ y_1'(t) & y_2'(t) \end{'{'}vmatrix{'}'}.$$ Enter the values of \(y_1\) and \(y_2\)
-       below to automatically calculate the Wronskian and Green's function!
-    </h6>
-  </Container>
-  <Container class="text-center">
-    <h6 class="mt-4 mb-4">
-      \(y_1(x) =\)
-      <MathInput bind:value={y_1} />
-      , \(y_2(x) =\)
-      <MathInput bind:value={y_2} />
-      <!-- , \(f(x) =\) -->
-      <!-- <MathInput bind:value={f} />
-      , \(x_0 =\)
-      <MathInput bind:value={x_0} /> -->
-
-      <br />
-      <br />
-      <Button class="yellow" on:click={submitInputs}>SUBMIT</Button>
-      <Button class="yellow" on:click={clearAll}>CLEAR</Button>
-      <br />
-      <br />
-      <Divider />
-
-      <br />
-      <strong>Wronskian \(W(y_1, y_2)\):</strong>
-      <MathDisplay value={w} />
-      <br />
-      <br />
-
-      <strong>Green's Function:</strong>
-      <MathDisplay value={answer} />
-      <!-- <br />
-      <br />
-      <strong>\(y_p\):</strong>
-      <MathDisplay value={answer2} /> -->
-
-    </h6>
-  </Container>
-</MaterialApp>
--- src/modules/calc.js
+++ src/modules/calc.js
@@ -36,19 +36,10 @@ export function GreensFctCalc(y1, y2, mode="tex") {
   const y2_ts = y2.replace(/x/g, "t");
   const W = Wronskian(y1, y2, "string");
  
-  console.log(W);
-  // -(2 * (sin(2 * t) ^ 2 + cos(2 * t) ^ 2))
   const Gs = `(${y1_ts} * ${y2_xs} - ${y1_xs} * ${y2_ts}) / (${W})`;
   const simp = simplifyExpression(parseExpression(Gs));
   
   var simp2 = simp.toString().replace("Infinity * ", "");
-  // console.log(simp2);
-  // (sin(2 t) * cos(2 x) - sin(2 x) * cos(2 t)) / -(2 * (sin(2 * t) ^ 2 + cos(2 * t) ^ 2))
-  // if (simp2.charAt(0) === "(" && simp2.charAt(simp2.length - 1) === ")") {
-  //   simp2 = simp2.substring(1, simp2.length - 1);
-  // }
-  console.log(simp2);
-  // sin(2 t) * cos(2 x) - sin(2 x) * cos(2 t)) / -(2 * (sin(2 * t) ^ 2 + cos(2 * t) ^ 2) WHERE_IS_THE_PARENTHESIS_TO_CLOSE
   const simp3 = parseExpression(simp2);
 
   return mode === "string" ? simp3.toString() : simp3.toTex();
@@ -69,33 +60,17 @@ export function Wronskian(y1, y2, mode="tex") {
   y2 = y2.replace(/\\right/g, "");
   y1 = y1.replace(/\\/g, "");
   y2 = y2.replace(/\\/g, "");
-  // const y1_xs = y1;
-  // const y2_xs = y2;
   const y1_ts = y1.replace(/x/g, "t");
   const y2_ts = y2.replace(/x/g, "t");
-
-  console.log(y1_ts, y2_ts);
 
   const y1_t = parseExpression(y1_ts);
   const y2_t = parseExpression(y2_ts);
 
   const Ws = `${y1_t.toString()} * ${derivative(y2_t, "t").toString()} - ${y2_t.toString()} * ${derivative(y1_t, "t").toString()}`;
-  var W = simplifyExpression(parseExpression(Ws)).toString();
+  let W = simplifyExpression(parseExpression(Ws)).toString();
   W = W.replace(/\s/g, "");
   W = W.replace(/2t/g, "2*t");
-  // W = W.replace("*(cos(2*t)^2+sin(2*t)^2))", "");
-  // "*(cos(2*t)^2+sin(2*t)^2))", ""
-  // "constcos(2*t)^2 constsin(2*t)^2))", ""
   W = parseExpression(W);
 
   return mode === "string" ? W.toString() : W.toTex();
 }
-
-// export function y_p(y1, y2, f, x_o) {
-//   const Gs = GreensFctCalc(y1, y2, "string");
-//   const integrand = simplifyExpression(parseExpression(`(${Gs}) * ${f}`)).toString();
-//   const i1 = integrand.replace(/t/g, "x");
-//   const i2 = integrand.replace(/t/g, `(${x_o})`);
-//   const yps = `(${i1}) - (${i2})`;
-//   return simplifyExpression(parseExpression(yps)).toTex();
-// }
