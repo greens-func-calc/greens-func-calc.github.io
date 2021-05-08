9f97582e62e172f88060128a07d05e7346c0a722
Kento Nishi <kento24gs@outlook.com>
2021-05-08 00:52:04 -0700

    Make solutions uneditable

--- src/components/MathDisplay.svelte
+++ src/components/MathDisplay.svelte
@@ -1,16 +1,19 @@
 <script>
-  import "mathquill/build/mathquill.js";
-  import "mathquill/build/mathquill.css";
+  import 'mathquill/build/mathquill.js';
+  import 'mathquill/build/mathquill.css';
   let inputElement;
   let mq;
-  export let value = "";
+  export let value = '';
   $: mq = MathQuill.getInterface(2).MathField(inputElement, {
     spaceBehavesLikeTab: true,
-    handlers: {}
+    handlers: {},
   });
   $: if (mq) {
     mq.latex(value);
   }
+  $: if (inputElement) {
+    inputElement.querySelectorAll('*').forEach((e) => (e.tabIndex = -1));
+  }
 </script>
 
-<span bind:this={inputElement} />
+<span bind:this={inputElement} style="pointer-events: none;" />
